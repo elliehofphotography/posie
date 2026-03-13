3f2b330d4ddfaf1b031027d07cef23b35d84b6f3
Ellie Hofheins <elliehofphotography@gmail.com>
2026-03-13 23:52:43 +0000

    File changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Search, Heart, Bookmark, TrendingUp } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import PullToRefresh from '../components/layout/PullToRefresh';
 
 const SAMPLE_CATEGORIES = [
   'Wedding', 'Couples', 'Portrait', 'Graduation', 'Maternity',
