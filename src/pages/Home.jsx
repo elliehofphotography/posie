45307c0563f79664aed3d53528f0103703257a49
Ellie Hofheins <elliehofphotography@gmail.com>
2026-03-18 18:23:35 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { Plus, Camera, Settings, Search, X, ShoppingBag, Shuffle } from 'lucide-react';
+import { Plus, Camera, Settings, Search, X, ShoppingBag, Shuffle, Images } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import TemplateCard from '../components/templates/TemplateCard';
