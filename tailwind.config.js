dd7cf7564f96714d592f6362c6fed0e367b1b1b2
Ellie Hofheins <elliehofphotography@gmail.com>
2026-03-18 17:42:10 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   	extend: {
   		fontFamily: {
   			inter: ['var(--font-inter)'],
-  			playfair: ['var(--font-playfair)', 'Georgia', 'cursive'],
+  			playfair: ['var(--font-playfair)', 'Georgia', 'serif'],
   			dm: ['var(--font-dm)', 'system-ui', 'sans-serif'],
   		},
   		borderRadius: {
