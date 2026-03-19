02dfedfb25b46fbed9fb5dc0d900995148dd1861
Ellie Hofheins <elliehofphotography@gmail.com>
2026-03-19 04:58:59 +0000

    File changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,6 +5,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 import { useNavigate } from 'react-router-dom';
 import PullToRefresh from '../components/layout/PullToRefresh';
+import PoseCategoryBar from '../components/ui/PoseCategoryBar';
 import AddDiscoverPostDialog from '../components/discover/AddDiscoverPostDialog';
 import DiscoverSaveToGalleryDialog from '../components/discover/DiscoverSaveToGalleryDialog';
 import ImageLightbox from '../components/ui/ImageLightbox';
