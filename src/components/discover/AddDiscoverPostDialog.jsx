c34191d4cd9ca4344ca358e36adfb9dbc155dc7a
Ellie Hofheins <elliehofphotography@gmail.com>
2026-03-19 05:13:44 +0000

    File changes

--- src/components/discover/AddDiscoverPostDialog.jsx
+++ src/components/discover/AddDiscoverPostDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
